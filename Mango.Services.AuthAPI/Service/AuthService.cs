64175299b5fe07149dbfbe0db2533a941ecc4165
Steve Kopecky <stevek357@hotmail.com>
2023-12-11 15:58:03 -0500

    Initial Commit

---
+++ Mango.Services.AuthAPI/Service/AuthService.cs
@@ -0,0 +1,34 @@
+﻿using Mango.Services.AuthAPI.Data;
+using Mango.Services.AuthAPI.Models;
+using Mango.Services.AuthAPI.Models.Dto;
+using Mango.Services.AuthAPI.Service.IService;
+using Microsoft.AspNetCore.Identity;
+
+namespace Mango.Services.AuthAPI.Service
+{
+    public class AuthService : IAuthService
+    {
+        public readonly AppDBContext _db;
+        public readonly UserManager<ApplicationUser> _userManger;
+        public readonly RoleManager<IdentityRole> _roleManger;
+
+        public AuthService(AppDBContext appDBContext, 
+            UserManager<ApplicationUser> userManager, 
+            RoleManager<IdentityRole> roleManager)
+        {
+            _db = appDBContext;
+            _userManger = userManager;
+            _roleManger = roleManager;
+        }
+
+        public Task<LoginRequestDto> Login(LoginRequestDto loginRequestDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserDto> Register(RegistrationRequestDto registrationRequestDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
+++ Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -9,7 +9,7 @@ using System.Collections.Generic;
 
 namespace Mango.Services.CouponAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/coupon")]
     [ApiController]
     public class CouponAPIController : ControllerBase
     {
@@ -118,6 +118,7 @@ namespace Mango.Services.CouponAPI.Controllers
         }
 
         [HttpDelete]
+        [Route("{id:int}")]
         public ResponseDto Delete(int id)
         {
             try
